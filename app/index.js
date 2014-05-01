6214354c446096be838aaedc44041d0b7065851c
tommy.fisher@creuna.fi <tommybfisher@gmail.com>
2014-05-01 22:07:10 +0300

    Wordpress integrated

--- app/index.js
+++ app/index.js
@@ -4,58 +4,115 @@ var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var appDir = 'app';
+var destDir = 'dist';
 
 
 var GulpJackGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
+    init: function () {
+        this.pkg = require('../package.json');
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
-      }
-    });
-  },
+        this.on('end', function () {
+            if (!this.options['skip-install']) {
+                this.installDependencies();
+            }
+        });
+    },
 
-  askFor: function () {
-    var done = this.async();
+    askFor: function () {
+        var done = this.async();
 
-    // have Yeoman greet the user
-    this.log(this.yeoman);
+        // have Yeoman greet the user
+        this.log(this.yeoman);
 
-    // replace it with a short and sweet description of your generator
-    this.log(chalk.magenta('You\'re using the fantastic GulpJack generator.'));
+        // replace it with a short and sweet description of your generator
+        this.log(chalk.magenta('You\'re using the fantastic GulpJack generator.'));
 
-    var prompts = [{
-        name: 'siteName',
-        message: 'What would you like to call your site?'
-      }];
+        var prompts = [{
+            name: 'siteName',
+            message: 'What would you like to call your site?'
+        }, {
+            type: 'confirm',
+            name: 'wordpress',
+            message: 'Is this site going to be running on WordPress?',
+            default: false
+        }, {
+            name: 'dbName',
+            message: 'What is the database name?',
+            default: 'wordpress',
+            when: function (props) {
+                return props.wordpress;
+            }
+        }, {
+            name: 'dbUsername',
+            message: 'What is the username for the database?',
+            default: 'root',
+            when: function (props) {
+                return props.wordpress;
+            }
+        }, {
+            name: 'dbPassword',
+            message: 'What is the password for the database',
+            default: 'root',
+            when: function (props) {
+                return props.wordpress;
+            }
+        }, {
+            name: 'dbHost',
+            message: 'What is the host for the database',
+            default: 'localhost',
+            when: function (props) {
+                return props.wordpress;
+            }
+        }, {
+            name: 'dbTablePrefix',
+            message: 'What is the table prefix',
+            default: 'wp_',
+            when: function (props) {
+                return props.wordpress;
+            }
+        }];
 
-    this.prompt(prompts, function (props) {
-      this.siteName = props.siteName;
+        this.prompt(prompts, function (props) {
+            this.siteName = props.siteName;
+            this.wordpress = props.wordpress;
+            this.dbName = props.dbName;
+            this.dbUsername = props.dbUsername;
+            this.dbPassword = props.dbPassword;
+            this.dbHost = props.dbHost;
+            this.dbTablePrefix = props.dbTablePrefix;
 
-      done();
-    }.bind(this));
-  },
+            done();
+        }.bind(this));
+    },
 
-  app: function () {
-    this.mkdir(appDir + '/assets');
-    this.mkdir(appDir + '/assets/images');
-    this.mkdir(appDir + '/assets/js/vendor');
+    app: function () {
+        if (this.wordpress) {
+            this.mkdir(destDir);
+            this.directory('wordpress/core', destDir);
+            this.copy('wordpress/_wp-config.php', destDir + '/wp-config.php');
+            this.mkdir(destDir + '/wp-content/themes/' + this._.slugify(this.siteName));
+            this.mkdir(appDir + '/images');
+            this.directory('wordpress/theme', appDir);
+            this.directory('sass', appDir + '/sass');
+        } else {
+            this.mkdir(appDir + '/assets');
+            this.mkdir(appDir + '/assets/images');
+            this.mkdir(appDir + '/assets/js/vendor');
 
-    this.directory('html', appDir);
-    this.directory('js', appDir + '/assets/js');
-    this.directory('sass', appDir + '/assets/sass');
+            this.directory('html', appDir);
+            this.directory('js', appDir + '/assets/js');
+            this.directory('sass', appDir + '/assets/sass');
+        }
 
-    this.copy('_package.json', 'package.json');
-    this.copy('_bower.json', 'bower.json');
-    this.copy('_gulpfile.js', 'gulpfile.js');
-  },
+        this.copy('_package.json', 'package.json');
+        this.copy('_bower.json', 'bower.json');
+        this.copy('_gulpfile.js', 'gulpfile.js');
+    },
 
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
-  }
+    projectfiles: function () {
+        this.copy('editorconfig', '.editorconfig');
+        this.copy('jshintrc', '.jshintrc');
+    }
 });
 
 module.exports = GulpJackGenerator;
--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -17,6 +17,7 @@ var imagemin = require('gulp-imagemin');
 var livereloadport = 35729,
     serverport = 5001;
 
+<% if (!wordpress) { %>
 var paths = {
     app: 'app',
     html: 'app/**/*.html',
@@ -30,6 +31,20 @@ var paths = {
     destCSS: 'dist/assets/css',
     destImg: 'dist/assets/images'
 };
+<% } %>
+
+<% if (wordpress) { %>
+var paths = {
+    php: 'app/**/*.php',
+    sass: 'app/sass/**/*.scss',
+    js: 'app/js/**/*.js',
+    img: 'app/images/**',
+    dest: 'dist/wp-content/themes/<%= _.slugify(siteName) %>',
+    destJS: 'dist/wp-content/themes/<%= _.slugify(siteName) %>/js',
+    destCSS: 'dist/wp-content/themes/<%= _.slugify(siteName) %>',
+    destImg: 'dist/wp-content/themes/<%= _.slugify(siteName) %>/images'
+};
+<% } %>
 
 var jsDepsToMove = [
     './bower_components/jquery-legacy/jquery.min.js',
@@ -44,12 +59,21 @@ gulp.task('vendorScripts', function () {
         .pipe(refresh(lrserver));
 });
 
+<% if (wordpress) { %>
+gulp.task('scripts', function () {
+    return gulp.src([paths.js])
+        .pipe(gulp.dest(paths.destJS))
+        .pipe(refresh(lrserver));
+});
+<% } %>
+<% if (!wordpress) { %>
 gulp.task('scripts', ['vendorScripts'], function () {
     return gulp.src([paths.js])
         .pipe(concat('script.js'))
         .pipe(gulp.dest(paths.destJS))
         .pipe(refresh(lrserver));
 });
+<% } %>
 
 gulp.task('styles', function () {
     return gulp.src(paths.sass)
@@ -70,7 +94,7 @@ gulp.task('moveJs', function () {
     .pipe(gulp.dest(paths.destJS + '/libs'));
 });
 
-gulp.task('cssToSass', ['move'], function () {
+gulp.task('cssToSass', <% if (!wordpress) { %>['move'], <% } %>function () {
     gulp.src('./bower_components/normalize.css/normalize.css')
     .pipe(rename('_normalize.scss'))
     .pipe(gulp.dest('./bower_components/normalize.css'));
@@ -82,7 +106,9 @@ gulp.task('move', [], function () {
 
 gulp.task('serve', function () {
     http.createServer(ecstatic({ root: __dirname + '/' + paths.dest })).listen(serverport);
+    <% if (!wordpress) { %>
     require('opn')('http://localhost:' + serverport);
+    <% } %>
     lrserver.listen(livereloadport);
 });
 
@@ -92,6 +118,15 @@ gulp.task('html', function () {
         .pipe(gulp.dest(paths.dest))
         .pipe(refresh(lrserver));
 });
+
+<% if (wordpress) { %>
+gulp.task('php', function () {
+    return gulp.src(paths.php)
+        .pipe(embedlr())
+        .pipe(gulp.dest(paths.dest))
+        .pipe(refresh(lrserver));
+});
+<% } %>
 
 gulp.task('images', function () {
     return gulp.src(paths.img)
@@ -105,9 +140,17 @@ gulp.task('watch', function () {
     gulp.watch(paths.sass, ['styles']);
     gulp.watch(paths.html, ['html']);
     gulp.watch(paths.img, ['images']);
+    <% if (wordpress) { %>
+    gulp.watch(paths.php, ['php']);
+    <% } %>
 });
 
+<% if (wordpress) { %>
+gulp.task('build', ['cssToSass', 'scripts', 'styles', 'php', 'images', 'serve', 'watch']);
+<% } %>
+<% if (!wordpress) { %>
 gulp.task('build', ['cssToSass', 'scripts', 'styles', 'html', 'images', 'serve', 'watch']);
+<% } %>
 
 gulp.task('default', ['clean'], function () {
     gulp.start('build');
