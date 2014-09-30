1cb5424ccbe2193950b7693ae43918e62062b506
Tommy Fisher <tommybfisher@gmail.com>
2014-09-30 10:25:01 +0300

    Reverted creating old browser polyfill script

--- app/templates/js/lib/modernizr.js
+++ app/templates/js/lib/modernizr.js
@@ -1,5 +1,5 @@
 /* Modernizr 2.8.3 (Custom Build) | MIT & BSD
- * Build: http://modernizr.com/download/#-fontface-backgroundsize-borderimage-borderradius-boxshadow-flexbox-flexboxlegacy-hsla-multiplebgs-opacity-rgba-textshadow-cssanimations-csscolumns-generatedcontent-cssgradients-cssreflections-csstransforms-csstransforms3d-csstransitions-applicationcache-canvas-canvastext-draganddrop-hashchange-history-audio-video-indexeddb-input-inputtypes-localstorage-postmessage-sessionstorage-websockets-websqldatabase-webworkers-geolocation-inlinesvg-smil-svg-svgclippaths-webgl-shiv-mq-cssclasses-addtest-prefixed-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-css_backgroundsizecover-css_boxsizing-css_calc-css_hyphens-css_lastchild-css_mediaqueries-css_pointerevents-css_positionsticky-forms_placeholder-load
+ * Build: http://modernizr.com/download/#-fontface-backgroundsize-borderimage-borderradius-boxshadow-flexbox-flexboxlegacy-hsla-multiplebgs-opacity-rgba-textshadow-cssanimations-csscolumns-generatedcontent-cssgradients-cssreflections-csstransforms-csstransforms3d-csstransitions-applicationcache-canvas-canvastext-draganddrop-hashchange-history-audio-video-indexeddb-input-inputtypes-localstorage-postmessage-sessionstorage-websockets-websqldatabase-webworkers-geolocation-inlinesvg-smil-svg-svgclippaths-webgl-shiv-mq-cssclasses-addtest-prefixed-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-css_backgroundsizecover-css_boxsizing-css_calc-css_lastchild-css_mediaqueries-css_pointerevents-css_positionsticky-forms_placeholder-load
  */
 ;
 
@@ -834,224 +834,6 @@ window.Modernizr = (function( window, document, undefined ) {
 /*yepnope1.5.4|WTFPL*/
 (function(a,b,c){function d(a){return"[object Function]"==o.call(a)}function e(a){return"string"==typeof a}function f(){}function g(a){return!a||"loaded"==a||"complete"==a||"uninitialized"==a}function h(){var a=p.shift();q=1,a?a.t?m(function(){("c"==a.t?B.injectCss:B.injectJs)(a.s,0,a.a,a.x,a.e,1)},0):(a(),h()):q=0}function i(a,c,d,e,f,i,j){function k(b){if(!o&&g(l.readyState)&&(u.r=o=1,!q&&h(),l.onload=l.onreadystatechange=null,b)){"img"!=a&&m(function(){t.removeChild(l)},50);for(var d in y[c])y[c].hasOwnProperty(d)&&y[c][d].onload()}}var j=j||B.errorTimeout,l=b.createElement(a),o=0,r=0,u={t:d,s:c,e:f,a:i,x:j};1===y[c]&&(r=1,y[c]=[]),"object"==a?l.data=c:(l.src=c,l.type=a),l.width=l.height="0",l.onerror=l.onload=l.onreadystatechange=function(){k.call(this,r)},p.splice(e,0,u),"img"!=a&&(r||2===y[c]?(t.insertBefore(l,s?null:n),m(k,j)):y[c].push(l))}function j(a,b,c,d,f){return q=0,b=b||"j",e(a)?i("c"==b?v:u,a,b,this.i++,c,d,f):(p.splice(this.i++,0,a),1==p.length&&h()),this}function k(){var a=B;return a.loader={load:j,i:0},a}var l=b.documentElement,m=a.setTimeout,n=b.getElementsByTagName("script")[0],o={}.toString,p=[],q=0,r="MozAppearance"in l.style,s=r&&!!b.createRange().compareNode,t=s?l:n.parentNode,l=a.opera&&"[object Opera]"==o.call(a.opera),l=!!b.attachEvent&&!l,u=r?"object":l?"script":"img",v=l?"script":u,w=Array.isArray||function(a){return"[object Array]"==o.call(a)},x=[],y={},z={timeout:function(a,b){return b.length&&(a.timeout=b[0]),a}},A,B;B=function(a){function b(a){var a=a.split("!"),b=x.length,c=a.pop(),d=a.length,c={url:c,origUrl:c,prefixes:a},e,f,g;for(f=0;f<d;f++)g=a[f].split("="),(e=z[g.shift()])&&(c=e(c,g));for(f=0;f<b;f++)c=x[f](c);return c}function g(a,e,f,g,h){var i=b(a),j=i.autoCallback;i.url.split(".").pop().split("?").shift(),i.bypass||(e&&(e=d(e)?e:e[a]||e[g]||e[a.split("/").pop().split("?")[0]]),i.instead?i.instead(a,e,f,g,h):(y[i.url]?i.noexec=!0:y[i.url]=1,f.load(i.url,i.forceCSS||!i.forceJS&&"css"==i.url.split(".").pop().split("?").shift()?"c":c,i.noexec,i.attrs,i.timeout),(d(e)||d(j))&&f.load(function(){k(),e&&e(i.origUrl,h,g),j&&j(i.origUrl,h,g),y[i.url]=2})))}function h(a,b){function c(a,c){if(a){if(e(a))c||(j=function(){var a=[].slice.call(arguments);k.apply(this,a),l()}),g(a,j,b,0,h);else if(Object(a)===a)for(n in m=function(){var b=0,c;for(c in a)a.hasOwnProperty(c)&&b++;return b}(),a)a.hasOwnProperty(n)&&(!c&&!--m&&(d(j)?j=function(){var a=[].slice.call(arguments);k.apply(this,a),l()}:j[n]=function(a){return function(){var b=[].slice.call(arguments);a&&a.apply(this,b),l()}}(k[n])),g(a[n],j,b,n,h))}else!c&&l()}var h=!!a.test,i=a.load||a.both,j=a.callback||f,k=j,l=a.complete||f,m,n;c(h?a.yep:a.nope,!!i),i&&c(i)}var i,j,l=this.yepnope.loader;if(e(a))g(a,0,l,0);else if(w(a))for(i=0;i<a.length;i++)j=a[i],e(j)?g(j,0,l,0):w(j)?B(j):Object(j)===j&&h(j,l);else Object(a)===a&&h(a,l)},B.addPrefix=function(a,b){z[a]=b},B.addFilter=function(a){x.push(a)},B.errorTimeout=1e4,null==b.readyState&&b.addEventListener&&(b.readyState="loading",b.addEventListener("DOMContentLoaded",A=function(){b.removeEventListener("DOMContentLoaded",A,0),b.readyState="complete"},0)),a.yepnope=k(),a.yepnope.executeStack=h,a.yepnope.injectJs=function(a,c,d,e,i,j){var k=b.createElement("script"),l,o,e=e||B.errorTimeout;k.src=a;for(o in d)k.setAttribute(o,d[o]);c=j?h:c||f,k.onreadystatechange=k.onload=function(){!l&&g(k.readyState)&&(l=1,c(),k.onload=k.onreadystatechange=null)},m(function(){l||(l=1,c(1))},e),i?k.onload():n.parentNode.insertBefore(k,n)},a.yepnope.injectCss=function(a,c,d,e,g,i){var e=b.createElement("link"),j,c=i?h:c||f;e.href=a,e.rel="stylesheet",e.type="text/css";for(j in d)e.setAttribute(j,d[j]);g||(n.parentNode.insertBefore(e,n),m(c,0))}})(this,document);
 Modernizr.load=function(){yepnope.apply(window,[].slice.call(arguments,0));};
-// last-child pseudo selector
-// https://github.com/Modernizr/Modernizr/pull/304
-
-
-Modernizr.addTest('lastchild', function(){
-
-  return Modernizr.testStyles("#modernizr div {width:100px} #modernizr :last-child{width:200px;display:block}", function (elem) {
-    return elem.lastChild.offsetWidth > elem.firstChild.offsetWidth;
-  }, 2);
-
-});
-
-
-Modernizr.addTest('mediaqueries', Modernizr.mq('only all'));/* see http://davidnewton.ca/the-current-state-of-hyphenation-on-the-web
-   http://davidnewton.ca/demos/hyphenation/test.html
-
-
-There are three tests:
-   1. csshyphens      - tests hyphens:auto actually adds hyphens to text
-   2. softhyphens     - tests that &shy; does its job
-   3. softhyphensfind - tests that in-browser Find functionality still works correctly with &shy;
-
-These tests currently require document.body to be present
-
-Hyphenation is language specific, sometimes.
-  See for more details: http://code.google.com/p/hyphenator/source/diff?spec=svn975&r=975&format=side&path=/trunk/Hyphenator.js#sc_svn975_313
-
-If loading Hyphenator.js via Modernizr.load, be cautious of issue 158: http://code.google.com/p/hyphenator/issues/detail?id=158
-
-More details at https://github.com/Modernizr/Modernizr/issues/312
-
-*/
-
-(function() {
-
-    if (!document.body){
-        window.console && console.warn('document.body doesn\'t exist. Modernizr hyphens test needs it.');
-        return;
-    }
-
-    // functional test of adding hyphens:auto
-    function test_hyphens_css() {
-        try {
-            /* create a div container and a span within that
-             * these have to be appended to document.body, otherwise some browsers can give false negative */
-            var div = document.createElement('div'),
-                span = document.createElement('span'),
-                divStyle = div.style,
-                spanHeight = 0,
-                spanWidth = 0,
-                result = false,
-                firstChild = document.body.firstElementChild || document.body.firstChild;
-
-            div.appendChild(span);
-            span.innerHTML = 'Bacon ipsum dolor sit amet jerky velit in culpa hamburger et. Laborum dolor proident, enim dolore duis commodo et strip steak. Salami anim et, veniam consectetur dolore qui tenderloin jowl velit sirloin. Et ad culpa, fatback cillum jowl ball tip ham hock nulla short ribs pariatur aute. Pig pancetta ham bresaola, ut boudin nostrud commodo flank esse cow tongue culpa. Pork belly bresaola enim pig, ea consectetur nisi. Fugiat officia turkey, ea cow jowl pariatur ullamco proident do laborum velit sausage. Magna biltong sint tri-tip commodo sed bacon, esse proident aliquip. Ullamco ham sint fugiat, velit in enim sed mollit nulla cow ut adipisicing nostrud consectetur. Proident dolore beef ribs, laborum nostrud meatball ea laboris rump cupidatat labore culpa. Shankle minim beef, velit sint cupidatat fugiat tenderloin pig et ball tip. Ut cow fatback salami, bacon ball tip et in shank strip steak bresaola. In ut pork belly sed mollit tri-tip magna culpa veniam, short ribs qui in andouille ham consequat. Dolore bacon t-bone, velit short ribs enim strip steak nulla. Voluptate labore ut, biltong swine irure jerky. Cupidatat excepteur aliquip salami dolore. Ball tip strip steak in pork dolor. Ad in esse biltong. Dolore tenderloin exercitation ad pork loin t-bone, dolore in chicken ball tip qui pig. Ut culpa tongue, sint ribeye dolore ex shank voluptate hamburger. Jowl et tempor, boudin pork chop labore ham hock drumstick consectetur tri-tip elit swine meatball chicken ground round. Proident shankle mollit dolore. Shoulder ut duis t-bone quis reprehenderit. Meatloaf dolore minim strip steak, laboris ea aute bacon beef ribs elit shank in veniam drumstick qui. Ex laboris meatball cow tongue pork belly. Ea ball tip reprehenderit pig, sed fatback boudin dolore flank aliquip laboris eu quis. Beef ribs duis beef, cow corned beef adipisicing commodo nisi deserunt exercitation. Cillum dolor t-bone spare ribs, ham hock est sirloin. Brisket irure meatloaf in, boudin pork belly sirloin ball tip. Sirloin sint irure nisi nostrud aliqua. Nostrud nulla aute, enim officia culpa ham hock. Aliqua reprehenderit dolore sunt nostrud sausage, ea boudin pork loin ut t-bone ham tempor. Tri-tip et pancetta drumstick laborum. Ham hock magna do nostrud in proident. Ex ground round fatback, venison non ribeye in.';
-
-            document.body.insertBefore(div, firstChild);
-
-            /* get size of unhyphenated text */
-            divStyle.cssText = 'position:absolute;top:0;left:0;width:5em;text-align:justify;text-justification:newspaper;';
-            spanHeight = span.offsetHeight;
-            spanWidth = span.offsetWidth;
-
-            /* compare size with hyphenated text */
-            divStyle.cssText = 'position:absolute;top:0;left:0;width:5em;text-align:justify;'+
-                                                 'text-justification:newspaper;'+
-                                                 Modernizr._prefixes.join('hyphens:auto; ');
-
-            result = (span.offsetHeight != spanHeight || span.offsetWidth != spanWidth);
-
-            /* results and cleanup */
-            document.body.removeChild(div);
-            div.removeChild(span);
-
-            return result;
-        } catch(e) {
-            return false;
-        }
-    }
-
-    // for the softhyphens test
-    function test_hyphens(delimiter, testWidth) {
-        try {
-            /* create a div container and a span within that
-             * these have to be appended to document.body, otherwise some browsers can give false negative */
-            var div = document.createElement('div'),
-                span = document.createElement('span'),
-                divStyle = div.style,
-                spanSize = 0,
-                result = false,
-                result1 = false,
-                result2 = false,
-                firstChild = document.body.firstElementChild || document.body.firstChild;
-
-            divStyle.cssText = 'position:absolute;top:0;left:0;overflow:visible;width:1.25em;';
-            div.appendChild(span);
-            document.body.insertBefore(div, firstChild);
-
-
-            /* get height of unwrapped text */
-            span.innerHTML = 'mm';
-            spanSize = span.offsetHeight;
-
-            /* compare height w/ delimiter, to see if it wraps to new line */
-            span.innerHTML = 'm' + delimiter + 'm';
-            result1 = (span.offsetHeight > spanSize);
-
-            /* if we're testing the width too (i.e. for soft-hyphen, not zws),
-             * this is because tested Blackberry devices will wrap the text but not display the hyphen */
-            if (testWidth) {
-                /* get width of wrapped, non-hyphenated text */
-                span.innerHTML = 'm<br />m';
-                spanSize = span.offsetWidth;
-
-                /* compare width w/ wrapped w/ delimiter to see if hyphen is present */
-                span.innerHTML = 'm' + delimiter + 'm';
-                result2 = (span.offsetWidth > spanSize);
-            } else {
-                result2 = true;
-            }
-
-            /* results and cleanup */
-            if (result1 === true && result2 === true) { result = true; }
-            document.body.removeChild(div);
-            div.removeChild(span);
-
-            return result;
-        } catch(e) {
-            return false;
-        }
-    }
-
-    // testing if in-browser Find functionality will work on hyphenated text
-    function test_hyphens_find(delimiter) {
-        try {
-            /* create a dummy input for resetting selection location, and a div container
-             * these have to be appended to document.body, otherwise some browsers can give false negative
-             * div container gets the doubled testword, separated by the delimiter
-             * Note: giving a width to div gives false positive in iOS Safari */
-            var dummy = document.createElement('input'),
-                div = document.createElement('div'),
-                testword = 'lebowski',
-                result = false,
-                textrange,
-                firstChild = document.body.firstElementChild || document.body.firstChild;
-
-            div.innerHTML = testword + delimiter + testword;
-
-            document.body.insertBefore(div, firstChild);
-            document.body.insertBefore(dummy, div);
-
-
-            /* reset the selection to the dummy input element, i.e. BEFORE the div container
-             *   stackoverflow.com/questions/499126/jquery-set-cursor-position-in-text-area */
-            if (dummy.setSelectionRange) {
-                dummy.focus();
-                dummy.setSelectionRange(0,0);
-            } else if (dummy.createTextRange) {
-                textrange = dummy.createTextRange();
-                textrange.collapse(true);
-                textrange.moveEnd('character', 0);
-                textrange.moveStart('character', 0);
-                textrange.select();
-            }
-
-            /* try to find the doubled testword, without the delimiter */
-            if (window.find) {
-                result = window.find(testword + testword);
-            } else {
-                try {
-                    textrange = window.self.document.body.createTextRange();
-                    result = textrange.findText(testword + testword);
-                } catch(e) {
-                    result = false;
-                }
-            }
-
-            document.body.removeChild(div);
-            document.body.removeChild(dummy);
-
-            return result;
-        } catch(e) {
-            return false;
-        }
-    }
-
-    Modernizr.addTest("csshyphens", function() {
-
-        if (!Modernizr.testAllProps('hyphens')) return false;
-
-        /* Chrome lies about its hyphens support so we need a more robust test
-                crbug.com/107111
-        */
-        try {
-            return test_hyphens_css();
-        } catch(e) {
-            return false;
-        }
-    });
-
-    Modernizr.addTest("softhyphens", function() {
-        try {
-            // use numeric entity instead of &shy; in case it's XHTML
-            return test_hyphens('&#173;', true) && test_hyphens('&#8203;', false);
-        } catch(e) {
-            return false;
-        }
-    });
-
-    Modernizr.addTest("softhyphensfind", function() {
-        try {
-            return test_hyphens_find('&#173;') && test_hyphens_find('&#8203;');
-        } catch(e) {
-            return false;
-        }
-    });
-
-})();
 
 // developer.mozilla.org/en/CSS/background-size
 
@@ -1061,7 +843,18 @@ Modernizr.testStyles( '#modernizr{background-size:cover}', function( elem ) {
         : elem.currentStyle;
 
     Modernizr.addTest( 'bgsizecover', style.backgroundSize == 'cover' );
-});// Method of allowing calculated values for length units, i.e. width: calc(100%-3em) http://caniuse.com/#search=calc
+});
+
+Modernizr.addTest('mediaqueries', Modernizr.mq('only all'));
+// developer.mozilla.org/en/CSS/box-sizing
+// github.com/Modernizr/Modernizr/issues/248
+
+Modernizr.addTest("boxsizing",function(){
+    return Modernizr.testAllProps("boxSizing") && (document.documentMode === undefined || document.documentMode > 7);
+});
+
+
+// Method of allowing calculated values for length units, i.e. width: calc(100%-3em) http://caniuse.com/#search=calc
 // By @calvein
 
 Modernizr.addTest('csscalc', function() {
@@ -1073,15 +866,17 @@ Modernizr.addTest('csscalc', function() {
 
     return !!el.style.length;
 });
+// last-child pseudo selector
+// https://github.com/Modernizr/Modernizr/pull/304
 
-// developer.mozilla.org/en/CSS/box-sizing
-// github.com/Modernizr/Modernizr/issues/248
 
-Modernizr.addTest("boxsizing",function(){
-    return Modernizr.testAllProps("boxSizing") && (document.documentMode === undefined || document.documentMode > 7);
+Modernizr.addTest('lastchild', function(){
+
+  return Modernizr.testStyles("#modernizr div {width:100px} #modernizr :last-child{width:200px;display:block}", function (elem) {
+    return elem.lastChild.offsetWidth > elem.firstChild.offsetWidth;
+  }, 2);
+
 });
-
-
 
 // developer.mozilla.org/en/CSS/pointer-events
 
